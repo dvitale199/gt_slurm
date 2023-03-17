57862297d2753e9e153cf7ad80ae4f9fd0a2e0f6
dvitale199 <d.vitale199@gmail.com>
2023-03-17 11:42:59 -0600

    adjusted home dir in call_gts.sh

--- cluster_scripts/call_gts.sh
+++ cluster_scripts/call_gts.sh
@@ -10,7 +10,7 @@ gcs_plink_path=$(echo "$line" | awk '{print $3}')
 echo "Starting job"
 date
 
-python3 /home/dan_datatecnica_com/scripts/call_gts.py --input ${idat_dir_in} --gcs_in ${gcs_idat_path} --gcs_out ${gcs_plink_path}
+python3 ~/scripts/call_gts.py --input ${idat_dir_in} --gcs_in ${gcs_idat_path} --gcs_out ${gcs_plink_path}
 
 echo "Job finished"
 date
