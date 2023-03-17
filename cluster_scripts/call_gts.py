4859b7dcd5170e24d376a80fbeac74c12f20cd2e
dvitale199 <d.vitale199@gmail.com>
2023-03-17 11:53:10 -0600

    adjusted gsutil cp ... to copy with *.idat

--- cluster_scripts/call_gts.py
+++ cluster_scripts/call_gts.py
@@ -14,7 +14,7 @@ home_dir = os.path.expanduser("~")
 os.makedirs(args.input, exist_ok=True)
 
 # Copy files from Google Cloud Storage to local directory
-os.system(f"gsutil cp gs://{args.gcs_in}/* {args.input}/")
+os.system(f"gsutil cp gs://{args.gcs_in}/*.idat {args.input}/")
 
 # Run iaap-cli command
 os.system(f"{home_dir}/GenoTools/executables/iaap-cli-linux-x64-1.1.0-sha.80d7e5b3d9c1fdfc2e99b472a90652fd3848bbc7/iaap-cli/iaap-cli gencall /home/dan_datatecnica_com/ilmn_files/NeuroBooster_20042459_A2.bpm /home/dan_datatecnica_com/ilmn_files/recluster_09272022.egt {args.input} -f {args.input} -p -t 4")
