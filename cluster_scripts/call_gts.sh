bc38234763a914848cb538a87b4ca192c7b6b777
dvitale199 <d.vitale199@gmail.com>
2023-03-28 14:44:02 -0600

    updates for more jobs in genocaller node

--- cluster_scripts/call_gts.sh
+++ cluster_scripts/call_gts.sh
@@ -4,13 +4,12 @@ line=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "${args_file}")
 
 # Parse the arguments from the line in the file
 idat_dir_in=$(echo "$line" | awk '{print $1}')
-gcs_idat_path=$(echo "$line" | awk '{print $2}')
-gcs_plink_path=$(echo "$line" | awk '{print $3}')
+out_path=$(echo "$line" | awk '{print $2}')
 
 echo "Starting job"
 date
 
-python3 ~/scripts/call_gts.py --input ${idat_dir_in} --gcs_in ${gcs_idat_path} --gcs_out ${gcs_plink_path}
+python3 ~/scripts/call_gts.py --input ${idat_dir_in} --output ${out_path}
 
 echo "Job finished"
 date
--- setup/setup.sh
+++ setup/setup.sh
@@ -3,30 +3,32 @@ git clone https://github.com/dvitale199/GenoTools.git
 cd GenoTools/
 pip3 install --user .
 cd ~/
-
+mkdir /genoslurm_nfs/bin
 
 # also run these in the compute node
 # get plink
 wget https://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20230116.zip
 unzip plink_linux_x86_64_20230116.zip
-mv plink /home/dan_datatecnica_com/bin/
+mv plink /genoslurm_nfs/bin/
 rm plink_linux_x86_64_20230116.zip prettify LICENSE toy.map toy.ped 
 
 # get plink2
-wget https://s3.amazonaws.com/plink2-assets/plink2_linux_avx2_20230109.zip
-unzip plink2_linux_avx2_20230109.zip
-mv plink2 /home/dan_datatecnica_com/bin/
+wget https://s3.amazonaws.com/plink2-assets/plink2_linux_avx2_20230303.zip
+unzip plink2_linux_avx2_20230303.zip
+mv plink2 /genoslurm_nfs/bin/
 rm plink2_linux_avx2_20230109.zip
 
 mkdir scripts
-mkdir data
+mkdir inputs
 mkdir logs
 
 # need to work out these versions later for VM
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+python3 -m pip install --upgrade matplotlib
 pip3 install --user h5py
 pip3 install --user Pillow
 pip3 install --user joblib
-pip3 install --user matplotlib
 pip3 install --user numpy
 pip3 install --user pandas
 pip3 install --user requests
@@ -36,10 +38,7 @@ pip3 install --user setuptools
 pip3 install --user statsmodels
 pip3 install --user umap
 
-# not working at the moment
-# pip3 install --user jupyter
 
-# mount gcs
-# gcsfuse --dir-mode 777 --file-mode 777 --implicit-dirs gp2_uk ~/gcs/gp2_data
-
-
+# some notes on installing GenoTools:
+# install will work but right now, importing gets stuck on downloading GCTA dynamically
+# will need to download locally and upload to /$HOME/.genotools/misc/executables/ to get this running
