72edaaaac7ae68655b6a29076239d5136d5d2dfb
dvitale199 <d.vitale199@gmail.com>
2023-04-04 10:43:28 -0600

    Revert "updates for more jobs in genocaller node"
    This reverts commit bc38234763a914848cb538a87b4ca192c7b6b777.

--- cluster_scripts/call_gts.py
+++ cluster_scripts/call_gts.py
@@ -1,35 +1,37 @@
 import os
 import argparse
-import sys
 
 parser = argparse.ArgumentParser(description='Process some files.')
-parser.add_argument('--input', type=str, help='Input directory containing idat files')
-parser.add_argument('--output', type=str, help='Output directory for plink bed/bim/fam files')
+parser.add_argument('--input', type=str, help='Local input directory')
+parser.add_argument('--gcs_in', type=str, help='Google Cloud Storage input directory')
+parser.add_argument('--gcs_out', type=str, help='Google Cloud Storage output directory')
 
 args = parser.parse_args()
 
 home_dir = os.path.expanduser("~")
-mount_dir = "/genoslurm_nfs"
-iaap = f"{home_dir}/GenoTools/executables/iaap-cli-linux-x64-1.1.0-sha.80d7e5b3d9c1fdfc2e99b472a90652fd3848bbc7/iaap-cli/iaap-cli"
+
+# Create directories
+os.makedirs(args.input, exist_ok=True)
+
+# Copy files from Google Cloud Storage to local directory
+os.system(f"gsutil cp gs://{args.gcs_in}/*.idat {args.input}/")
 
 # Run iaap-cli command
-os.system(f"{iaap} gencall {mount_dir}/ilmn_files/NeuroBooster_20042459_A2.bpm {mount_dir}/ilmn_files/recluster_09272022.egt {args.output} -f {args.input} -p -t 4")
+os.system(f"{home_dir}/GenoTools/executables/iaap-cli-linux-x64-1.1.0-sha.80d7e5b3d9c1fdfc2e99b472a90652fd3848bbc7/iaap-cli/iaap-cli gencall /home/dan_datatecnica_com/ilmn_files/NeuroBooster_20042459_A2.bpm /home/dan_datatecnica_com/ilmn_files/recluster_09272022.egt {args.input} -f {args.input} -p -t 4")
 
 # Convert PED files to BED/BIM/FAM files using plink
-for ped_file in os.listdir(args.output):
+for ped_file in os.listdir(args.input):
     if ped_file.endswith(".ped"):
-        ped_file_path = os.path.join(args.output, ped_file)
-        out_file_path = os.path.join(args.output, os.path.splitext(ped_file)[0])
+        ped_file_path = os.path.join(args.input, ped_file)
+        out_file_path = os.path.join(args.input, os.path.splitext(ped_file)[0])
         ped_in = ped_file_path.split('.')[0]
-        cp_cmd = f"cp {args.output}/NeuroBooster_20042459_A2.map {out_file_path}.map"
+        cp_cmd = f"cp {args.input}/NeuroBooster_20042459_A2.map {out_file_path}.map"
         os.system(cp_cmd)
-        plink_cmd = f"{mount_dir}/bin/plink --file {ped_in} --make-bed --out {out_file_path}"
+        plink_cmd = f"{home_dir}/bin/plink --file {ped_in} --make-bed --out {out_file_path}"
         os.system(plink_cmd)
-        # if output bed exists, delete ped/map files otherwise print error to stderr
-        if os.path.exists(f"{out_file_path}.bed"):
-            rm_cmd = f"rm {ped_in}.{{ped,map}}"
-            os.system(rm_cmd)
-        else:
-            # print error to stderr
-            print(f"Error: {out_file_path}.bed does not exist", file=sys.stderr)
 
+# Copy output files back to Google Cloud Storage
+os.system(f"gsutil cp {args.input}/*.{{bed,bim,fam,log}} gs://{args.gcs_out}/")
+
+# now remove input directory in vm
+os.system(f"rm -rf {args.input}")
--- cluster_scripts/call_gts.sh
+++ cluster_scripts/call_gts.sh
@@ -4,12 +4,13 @@ line=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "${args_file}")
 
 # Parse the arguments from the line in the file
 idat_dir_in=$(echo "$line" | awk '{print $1}')
-out_path=$(echo "$line" | awk '{print $2}')
+gcs_idat_path=$(echo "$line" | awk '{print $2}')
+gcs_plink_path=$(echo "$line" | awk '{print $3}')
 
 echo "Starting job"
 date
 
-python3 ~/scripts/call_gts.py --input ${idat_dir_in} --output ${out_path}
+python3 ~/scripts/call_gts.py --input ${idat_dir_in} --gcs_in ${gcs_idat_path} --gcs_out ${gcs_plink_path}
 
 echo "Job finished"
 date
